8981ecf3b7dcf0c23c8a0733363c007d154b76a8
Kim-Myoung-jae <rlaaud30@naver.com>
2021-05-14 02:26:22 +0900

    내 위치 기반 1km 이내에 있는 주변 식당 정보 표시

--- app/src/main/java/com/example/selectfoods/KakaoAPI.kt
+++ app/src/main/java/com/example/selectfoods/KakaoAPI.kt
@@ -11,12 +11,12 @@ interface KakaoAPI {
     @GET("/v2/local/search/keyword.json") //Keyword.json의 정보를 받음
     fun getSearchKeyword (
         @Header("Authorization") key: String, //카카오 API 인증키 (필수)
-        @Query("query") query: String //검색할 질의어 (필수)
+        @Query("query") query: String, //검색할 질의어 (필수)
         //매개변수 추가~ 내 주변으로 식당 구해야 하니까 카테고리 그룹, x,y, radius
-     /*   @Query("category_group_code") category: String,
+        //@Query("category_group_code") category: String,
         @Query("x") x:String,
         @Query("y") y:String,
-        @Query("radius") radius:Int*/
+        @Query("radius") radius:Int //단위 미터
 
     ): Call<ResultSearchKeyword> //받아온 정보가 ResultSearchKeyword 클래스의 구조로 담김
 
